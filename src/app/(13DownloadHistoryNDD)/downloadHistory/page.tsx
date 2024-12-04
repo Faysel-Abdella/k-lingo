7c0335d83ff6013192d7e486c2b8916ef9b61f54
Faysel Abdella <fayselcode@gmail.com>
2024-12-04 13:48:31 +0300

    Fix download history issue

--- src/app/(13DownloadHistoryNDD)/downloadHistory/page.tsx
+++ src/app/(13DownloadHistoryNDD)/downloadHistory/page.tsx
@@ -1,10 +1,7 @@
-// eslint-disable react-hooks/exhaustive-deps
-
 "use client";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Button, Card, CardBody } from "@nextui-org/react";
 import Image from "next/image";
 import { useState, useMemo } from "react";
 import { DownloadHistory } from "@/assets/downloadHistoryT1";
@@ -17,13 +14,12 @@ import {
   TableCell,
   Pagination,
 } from "@nextui-org/react";
+import React from "react";
 
 const TicketAndPaymentDetails = () => {
   // Pagination Logic
   const [page, setPage] = useState(1);
-
-  const rowsPerPage = 15;
-
+  const rowsPerPage = 20;
   const pages = Math.ceil(DownloadHistory.length / rowsPerPage);
 
   const [currentData, setCurrentData] = useState<any>();
@@ -34,7 +30,22 @@ const TicketAndPaymentDetails = () => {
 
     setCurrentData(DownloadHistory.slice(start, end));
     return DownloadHistory.slice(start, end);
-  }, [page, rowsPerPage]);
+  }, [page, rowsPerPage, DownloadHistory]);
+
+  const firstHalf = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return DownloadHistory.slice(start, end);
+  }, [page, rowsPerPage, DownloadHistory]);
+
+  const secondHalf = useMemo(() => {
+    const start = page * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return DownloadHistory.slice(start, end);
+  }, [page, rowsPerPage, DownloadHistory]);
+
   return (
     <div className="min-h-[1117px] flex flex-col items-center justify-between">
       {/* Include Header */}
@@ -73,76 +84,146 @@ const TicketAndPaymentDetails = () => {
           </div>
 
           <div className="w-full min-h-[484px]  max-md:overflow-x-scroll">
-            <article className="m-0">
-              <Table
-                aria-label="Data Table"
-                shadow="none"
-                classNames={{
-                  th: [
-                    "relative font-bold text-[14px] bg-[#F3F4F6] text-[#868F9A] h-[44px] text-center",
-                    "after:content-[''] after:absolute after:right-0 after:top-2 after:bottom-2 after:w-[1px] after:bg-gray-300",
-                  ],
-                  td: ["px-6 text-center font-normal text-base text-[#363941]"],
-                }}
-                bottomContent={
-                  <div className="flex w-full justify-center mt-8">
-                    <Pagination
-                      isCompact
-                      showControls
-                      showShadow
-                      color="primary"
-                      page={page}
-                      total={pages}
-                      onChange={(page) => setPage(page)}
-                    />
-                  </div>
-                }
-              >
-                <TableHeader>
-                  <TableColumn>번호</TableColumn>
-                  <TableColumn>제목</TableColumn>
-                  <TableColumn>작성자</TableColumn>
-                  <TableColumn>쪽수</TableColumn>
-                  <TableColumn>연회원 점수</TableColumn>
-                  <TableColumn>포인트</TableColumn>
-                  <TableColumn>재다운로드</TableColumn>
-                </TableHeader>
-                <TableBody>
-                  {items.map((row) => (
-                    <TableRow key={row.id} className="border-b-1">
-                      <TableCell>{row.number}</TableCell>
-                      <TableCell>
-                        <div className="flex items-center justify-center gap-2">
-                          <span className="text-nowrap">{row.title}</span>
-                          <Image
-                            src="/assets/Icons/Attach.svg"
-                            alt="Clip Image"
-                            width={12}
-                            height={12}
-                          />
-                        </div>
-                      </TableCell>
-                      <TableCell className="text-nowrap">
-                        {row.author}
-                      </TableCell>
-                      <TableCell>{row.numberOfPages}</TableCell>
-                      <TableCell>{row.annualMemberScore}</TableCell>
-                      <TableCell>{row.point}</TableCell>
-                      <TableCell>
-                        <div className="flex items-center justify-center gap-2">
-                          <span className="text-[#42A8FD] text-nowrap">
-                            {row.reDownload}
-                          </span>
-                          <span className="text-[10px] text-[#868F9A]">
-                            {" "}
-                            (~10.30){" "}
-                          </span>
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+            <article className="m-0 ">
+              {DownloadHistory.length <= 21 && (
+                <Table
+                  aria-label="Data Table"
+                  shadow="none"
+                  classNames={{
+                    th: [
+                      "relative font-bold text-[14px] bg-[#F3F4F6] text-[#868F9A] h-[44px] text-center",
+                      "after:content-[''] after:absolute after:right-0 after:top-2 after:bottom-2 after:w-[1px] after:bg-gray-300",
+                    ],
+                    td: [
+                      "px-6 text-center font-normal text-base text-[#363941]",
+                    ],
+                  }}
+                  bottomContent={
+                    <div className="flex w-full justify-center mt-8">
+                      <Pagination
+                        isCompact
+                        showControls
+                        showShadow
+                        color="primary"
+                        page={page}
+                        total={pages}
+                        onChange={(page) => setPage(page)}
+                      />
+                    </div>
+                  }
+                >
+                  <TableHeader>
+                    <TableColumn>번호</TableColumn>
+                    <TableColumn>제목</TableColumn>
+                    <TableColumn>다운로드일</TableColumn>
+                  </TableHeader>
+                  <TableBody>
+                    {DownloadHistory.map((row) => (
+                      <TableRow key={row.id} className="border-b-1">
+                        <TableCell>{row.number}</TableCell>
+                        <TableCell>
+                          <div className="flex items-center justify-center gap-2">
+                            <span className="text-nowrap">{row.title}</span>
+                            <Image
+                              src="/assets/Icons/Attach.svg"
+                              alt="Clip Image"
+                              width={12}
+                              height={12}
+                            />
+                          </div>
+                        </TableCell>
+                        <TableCell className="text-nowrap">
+                          {row.downloadDate}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
+
+              {DownloadHistory.length >= 22 && (
+                <Table
+                  aria-label="Data Table"
+                  shadow="none"
+                  classNames={{
+                    th: [
+                      "relative font-bold text-[14px] bg-[#F3F4F6] text-[#868F9A] h-[44px] text-center",
+                      "after:content-[''] after:absolute after:right-0 after:top-2 after:bottom-2 after:w-[1px] after:bg-gray-300",
+                    ],
+                    td: [
+                      "px-6 text-center font-normal text-base text-[#363941]",
+                    ],
+                  }}
+                  bottomContent={
+                    <div className="flex w-full justify-center mt-8">
+                      <Pagination
+                        isCompact
+                        showControls
+                        showShadow
+                        color="primary"
+                        page={page}
+                        total={pages}
+                        onChange={(page) => setPage(page)}
+                      />
+                    </div>
+                  }
+                >
+                  <TableHeader>
+                    <TableColumn>번호</TableColumn>
+                    <TableColumn>제목</TableColumn>
+                    <TableColumn>다운로드일</TableColumn>
+                    {/* Second main column */}
+                    <TableColumn className="pl-20">번호</TableColumn>
+                    <TableColumn>제목</TableColumn>
+                    <TableColumn>다운로드일</TableColumn>
+                  </TableHeader>
+                  <TableBody>
+                    {firstHalf.map((row, index) => (
+                      <TableRow key={row.id} className="border-b-1">
+                        <TableCell>{row.number}</TableCell>
+                        <TableCell>
+                          <div className="flex items-center justify-center gap-2">
+                            <span className="text-nowrap">{row.title}</span>
+                            <Image
+                              src="/assets/Icons/Attach.svg"
+                              alt="Clip Image"
+                              width={12}
+                              height={12}
+                            />
+                          </div>
+                        </TableCell>
+                        <TableCell className="text-nowrap">
+                          {row.downloadDate}
+                        </TableCell>
+
+                        {/* Second main column */}
+
+                        <TableCell className="pl-20">
+                          {secondHalf[index] && secondHalf[index].number}
+                        </TableCell>
+                        <TableCell>
+                          {secondHalf[index] && (
+                            <div className="flex items-center justify-center gap-2">
+                              <span className="text-nowrap">
+                                {secondHalf[index].title}
+                              </span>
+                              <Image
+                                src="/assets/Icons/Attach.svg"
+                                alt="Clip Image"
+                                width={12}
+                                height={12}
+                              />
+                            </div>
+                          )}
+                        </TableCell>
+                        <TableCell className="text-nowrap">
+                          {secondHalf[index] && secondHalf[index].downloadDate}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
             </article>
           </div>
         </div>
